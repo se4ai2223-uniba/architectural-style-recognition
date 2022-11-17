e7fae490e1288864fd5aa4b6f3ebb79fa1c3509e
AndreaBasile97 <andreabasile97@gmail.com>
2022-11-17 15:20:12 +0100

    validate file image

--- src/api/send_file.py
+++ src/api/send_file.py
@@ -2,9 +2,11 @@ import requests
 import os
 
 def post_file(endpoint, filepath):
-    file = {'file': open(filepath, 'rb')}
-    headers = {'content-type' : 'image/jpg'}
-    resp = requests.post(url=endpoint, files=file)
-    print(resp.json())
-
+    filename, file_extension = os.path.splitext(filepath)
+    if(str(file_extension).lower() in ['.jpg', 'bmp', 'gif', 'png']):
+        file = {'file': open(filepath, 'rb')}
+        resp = requests.post(url=endpoint, files=file)
+        print(resp.json())
+    else:
+        print("File non riconosciuto")
 post_file('http://127.0.0.1:8000/uploadimage/', '../../data/processed/arcDatasetSelected/Achaemenid architecture/21_Tonbeaux-achemenides.JPG')
