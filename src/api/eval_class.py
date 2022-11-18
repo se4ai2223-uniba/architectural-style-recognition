740d118a8a210fb5ba1b7278710b79cf074751d3
aiacovazzi <ar.iacovazzi@gmail.com>
2022-11-18 14:53:09 +0100

    fixed eval

--- src/api/eval_class.py
+++ src/api/eval_class.py
@@ -4,14 +4,14 @@ import pandas as pd
 import os
 
 app = FastAPI()
-dataset_json = os.path.join('data','json','dataset.json')
-classification_json = os.path.join('data','json','classification.json')
+dataset_csv = os.path.join('..','..','data','external','dataset.csv')
+classification_csv = os.path.join('..','..','data','external','classification.csv')
 
 def evaluate_classification(id, classification):
     idsd = []
-    dfc = pd.read_json(classification_json)
+    dfc = pd.read_csv(classification_csv)
     
-    dfd = pd.read_json(dataset_json)
+    dfd = pd.read_csv(dataset_csv)
     if not dfc.empty:
         idsc = dfc["id_img"].to_list()
     if not dfd.empty:
@@ -19,10 +19,9 @@ def evaluate_classification(id, classification):
     if (id in idsc and id not in idsd):
         dfd_ins = pd.DataFrame([[id, classification]], columns=['id_img', 'classification'])        
         dfd=pd.concat([dfd, dfd_ins])  
-        parsed = json.loads(dfd.to_json(orient='records'))
-        f = open(dataset_json,'w')
-        json.dump(parsed , f, indent=4)
-        f.close()
+
+        print(dfd)
+        dfd.to_csv(dataset_csv, index=False)  
         return 'ok'
     else:
         return 'ko'
@@ -31,5 +30,3 @@ def evaluate_classification(id, classification):
 async def eval_class( id_img: int, new_class: str):
     res = evaluate_classification(id_img, new_class)
     return {"result": res}
-
-evaluate_classification(2, 'bobo')
