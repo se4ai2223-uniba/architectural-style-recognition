493d1dac3a409538537358f3341f03da274a4fca
Roberto <roblorus@icloud.com>
2022-11-20 03:25:51 +0100

    UploadImage endpoint added, code refactoring, added dictionary of the labels

--- src/api/classify.py
+++ src/api/classify.py
@@ -1,66 +1,39 @@
-from http.client import HTTPException
-from fastapi import FastAPI, File, UploadFile
-import tensorflow as tf
-import tensorflow_hub as hub
+from fastapi import UploadFile
 import numpy as np
-from tensorflow.keras.preprocessing import image #type:ignore
-from tensorflow.keras.models import model_from_json #type:ignore
 import os
-import csv
 from id_manager import read_id, increase_id
+from utils import *
+import pandas as pd
 
-
-#riceve un'immagine e fornisce la predizione. l'immagine e la predizione vengono salvate.
+# riceve un'immagine e fornisce la predizione. l'immagine e la predizione vengono salvate.
 
 # refactoring e modularizzzazoine
-async def upload_img(file: UploadFile):
+async def do_predict(file: UploadFile, model):
+
     contents = await file.read()
     nuovo_id = read_id()
-    image_path = os.path.join('..','..','data','external', 'images', str(nuovo_id) + "_" + file.filename)
+    image_path = os.path.join(
+        "..", "..", "data", "external", "images", str(nuovo_id) + "_" + file.filename
+    )
     with open(image_path, "wb") as f:
         f.write(contents)
     print("File Uploaded")
-    path_saved_model = os.path.join('..','..','models','saved-model-optimal/')
-    model = load_model(path_saved_model)
+
     preprocessed_image = prepare_image(image_path)
+
     predictions = model.predict(preprocessed_image)
     label = np.argmax(predictions)
-    insert_into_csv(os.path.join('..','..','data','external', 'predictions.csv'), str(nuovo_id), str(label))
+
+    insert_into_csv(
+        os.path.join("..", "..", "data", "external", "predictions.csv"),
+        str(nuovo_id),
+        str(label),
+    )
     increase_id()
-    return {"filename": file.filename, "id":str(nuovo_id), "label": str(label)}
 
-#carica il modello allenato dal path
+    if os.path.exists(os.path.join("..", "..", "data", "external", "dictionary.csv")):
+        df = pd.read_csv(os.path.join("..", "..", "data", "external", "dictionary.csv"))
+        label = df[str(label)]
+        del df
 
-# Caricare modello nel server
-def load_model(path):
-    model_loaded = tf.keras.Sequential()
-    # load json and create model
-    json_file = open(path+'model.json', 'r')
-    model_json = json_file.read()
-    json_file.close()
-    model_loaded = model_from_json(model_json, custom_objects={'KerasLayer': hub.KerasLayer})
-    # load weights into new model
-    model_loaded.load_weights(path+"model.h5")
-    return model_loaded
-
-#prepara l'immagine in maniera che sia accettabile dalla CNN
-def prepare_image(file):
-    img = image.load_img(file, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
-
-#Inserisce una predizione nel csv
-def insert_into_csv(path_json_file, id_image, label):
-    header = ['id_img', 'id_class']
-    pred = [id_image, label]
-
-    if(not os.path.exists(path_json_file)):
-        with open(path_json_file, 'a', newline='') as predictions_file:
-            w=csv.writer(predictions_file)
-            w.writerow(header)
-    with open(path_json_file, 'a', newline='') as predictions_file:
-        w=csv.writer(predictions_file)
-        w.writerow(pred)
-
-    
+    return {"filename": file.filename, "id": str(nuovo_id), "label": str(label)}
--- src/api/server.py
+++ src/api/server.py
@@ -1,17 +1,52 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-# Caricare modello qui e passarlo alla funzione per la classificazione
-
-# Costruire dizionario per mappare numeri
-
+from src.models.model import Model
 from classify import *
 from eval_class import *
+import shutil
+from typing import Union
 
 
-@app.post("/uploadimage/")
-async def create_upload_image(file: UploadFile):
-    return await upload_img(file)
+path_saved_model = os.path.join("..", "..", "models", "saved-model-optimal")
+
+## remove the parameter cur_path if appears the error "No such file 'params.yaml'"
+model = Model(cur_path=os.path.join("..", ".."))
+
+model = model.loadModel(path_saved_model)
+
+
+app = FastAPI()
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile, label: str):
+
+    # conteollare che label sia nel giusto range e decidere se debba essere testuale o numerica
+    try:
+        contents = await file.read()
+        new_id = read_id()
+        image_path = os.path.join(
+            "..", "..", "data", "external", "images", str(new_id) + "_" + file.filename
+        )
+        with open(image_path, "wb") as f:
+            await f.write(contents)
+        print("File Uploaded")
+
+        insert_into_csv(
+            os.path.join("..", "..", "data", "external", "dataset.csv"),
+            str(new_id),
+            str(label),
+        )
+        increase_id()
+    except:
+        print("FastAPI: Exception thrown in uploadimage endpoint")
+    finally:
+        file.file.close()
+        return {"filename": file.filename, "Label": label}
+
+
+@app.post("/predict/")
+async def predict(file: UploadFile):
+    return await do_predict(file, model)
 
 
 # creare endpoint per classificazione
