1be179d300d218e5d1daef1078c185d79254b20d
Andrea Basile <andrea.basile@apuliasoft.com>
2022-11-22 19:44:56 +0100

    E fattill tu

--- src/api/server.py
+++ src/api/server.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from pydantic import BaseModel, ValidationError, validator
 from src.models.model import Model
 from classify import *
 from eval_class import *
 from upfile import *
 import cv2
+import json
+import PIL
+from PIL import Image
+import io
 path_saved_model = os.path.join("..", "..", "models", "saved-model-optimal")
 
 ## remove the parameter cur_path if appears the error "No such file 'params.yaml'"
@@ -13,31 +17,46 @@ model = model.loadModel(path_saved_model)
 app = FastAPI()
 
 
-class ImageModel(BaseModel):
-    image: UploadFile
-    @validator('image')
-    def check_image(cls, image):
+class PredictPayload(BaseModel):
+
+    # Same name as parameter of body request
+    maybeImage: UploadFile = File(...)
+
+    @validator("*")
+    def is_image(cls, v):
+        contents = v.file.read()
         try:
-            img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-        except:
-            raise ValueError('File is not an image')
+            Image.open(io.BytesIO(contents))
+        except PIL.UnidentifiedImageError:
+            return "Image needed!"
+
+        return contents
 
 class LabelModel(BaseModel):
     val: int
-    @validator('val')
-    def check_val(cls, v):
-        if not (v >= 0 and v < 10):
-            raise ValueError('Mamt!')
 
 @app.post("/uploadfile/")
-async def upload_file(file: UploadFile, label: LabelModel):
-    res = await do_upload(file, label.val)
-    return res
+async def upload_file(file: UploadFile, label: int):
+    try:
+        labelModel = LabelModel(
+        val = label
+        )
+        res = await do_upload(file, label)
+        return res    
+    except ValidationError as e:
+        raise HTTPException(status_code=406, detail=str(e.raw_errors[0].exc))
 
 @app.post("/predict/")
 async def predict(file: UploadFile):
-    res = await do_predict(file, model)
-    return res
+    try:
+        img = PredictPayload(
+            maybeImage = file
+        )
+        res = await do_predict(file, model)
+        return res
+    except ValidationError as e:
+        raise HTTPException(status_code=406, detail=str(e.raw_errors[0].exc))
+
 
 @app.post("/eval_class/")
 async def eval_class(id_img: int, new_class: int):
