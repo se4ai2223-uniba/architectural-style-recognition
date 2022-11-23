829720c93d4b978713dbae6d19db47e1fc4895ea
AndreaBasile97 <andreabasile97@gmail.com>
2022-11-23 02:46:48 +0100

    first test

--- src/api/test_server.py
+++ src/api/test_server.py
@@ -1,14 +1,14 @@
 from fastapi.testclient import TestClient
 from utils import read_id
 
-from .server import app
+from server import app
 client = TestClient(app)
 
 # testare che dando un immagine
 def test_prediction():
-    image_file = 'data\processed\arcDatasetSelected\Achaemenid architecture\21_Tonbeaux-achemenides.JPG'
-    files = {'file': ('image', open(image_file, 'rb'))}
+    image_file = '../../data/processed/arcDatasetSelected/Achaemenid architecture/21_Tonbeaux-achemenides.JPG'
+    files = {'imgfile': ('21_Tonbeaux-achemenides.JPG', open(image_file, 'rb'))}
     response = client.post("/predict/", files=files)
     nuovo_id = read_id()
     assert response.status_code == 200
-    assert response.json() == {"filename": '21_Tonbeaux-achemenides.JPG', "id": nuovo_id, "label": 0}
+    assert response.json() == {"filename": '21_Tonbeaux-achemenides.JPG', "id": str(nuovo_id-1), "label": 'ancient egyptian architecture'}
