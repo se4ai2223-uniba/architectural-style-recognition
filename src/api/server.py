df6357016406e924c87a9a7c9838825a5ae9e95a
Roberto <roblorus@icloud.com>
2022-12-15 01:22:38 +0100

    Fix to frontend calls to API services

--- src/api/server.py
+++ src/api/server.py
@@ -1,4 +1,4 @@
-'''This module implements the behaviour of the endpoints of the API'''
+"""This module implements the behaviour of the endpoints of the API"""
 import os
 import io
 import copy
@@ -20,10 +20,7 @@ model = model.loadModel(path_saved_model)
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:81",
-    "http://localhost",
-]
+origins = ["http://localhost:9200", "http://localhost:9100", "http://localhost"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -34,38 +31,43 @@ app.add_middleware(
 )
 
 
-
 class ImageValidator(BaseModel):
-    '''Pydantic validator for images'''
+    """Pydantic validator for images"""
+
     # Same name as parameter of body request
     image: UploadFile
 
     @validator("image")
     def check_image(cls, image):
-        '''Checks that the input file is actually an image'''
+        """Checks that the input file is actually an image"""
         img = image.file.read()
         try:
             Image.open(io.BytesIO(img))
             image.file.close()
         except PIL.UnidentifiedImageError as exc:
-            raise ValueError("Image upload error, the file provided is not an image.") from exc
+            raise ValueError(
+                "Image upload error, the file provided is not an image."
+            ) from exc
         return img
 
+
 class LabelValidator(BaseModel):
-    '''Pydantic validator for class labels'''
+    """Pydantic validator for class labels"""
+
     val: int
 
     @validator("val")
     def check_val(cls, value):
-        '''Checks that the provided label is between 0 and 9'''
+        """Checks that the provided label is between 0 and 9"""
         if not 0 <= value < 10:
             raise ValueError(
                 "Id label error, the label must be a value between 0 and 9."
             )
 
+
 @app.post("/extend_dataset/")
 async def upload_file(imgfile: UploadFile, label: int):
-    '''Upload an image in order to expand the dataset'''
+    """Upload an image in order to expand the dataset"""
     try:
         LabelValidator(val=label)
         ImageValidator(image=copy.deepcopy(imgfile))
@@ -74,9 +76,10 @@ async def upload_file(imgfile: UploadFile, label: int):
     except ValidationError as exc:
         raise HTTPException(status_code=406, detail=str(exc.raw_errors[0].exc)) from exc
 
+
 @app.post("/classify_image/")
 async def predict(imgfile: UploadFile):
-    '''Use the ml model in order to classify an image'''
+    """Use the ml model in order to classify an image"""
     try:
         ImageValidator(image=copy.deepcopy(imgfile))
         res = await do_predict(imgfile, model)
@@ -84,9 +87,10 @@ async def predict(imgfile: UploadFile):
     except ValidationError as exc:
         raise HTTPException(status_code=406, detail=str(exc.raw_errors[0].exc)) from exc
 
+
 @app.put("/feedback_class/")
 async def eval_class(id_img: int, new_class: int):
-    '''Allows experts to give the real label of an image already classified'''
+    """Allows experts to give the real label of an image already classified"""
     try:
         LabelValidator(val=new_class)
         res = evaluate_classification(id_img, new_class)
--- src/frontend/static/expert/expert-form-script.js
+++ src/frontend/static/expert/expert-form-script.js
@@ -2,94 +2,97 @@ var id_image
 var new_label
 
 function readURL(input) {
-    if (input.files && input.files[0]) {
-  
-      var reader = new FileReader();
-  
-      reader.onload = function(e) {
-        $('.image-upload-wrap').hide();
-  
-        $('.file-upload-image').attr('src', e.target.result);
-        $('.file-upload-content').show();
-  
-        $('.image-title').html(input.files[0].name);
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-      upload(input.files[0])
-  
-    } else {
-      removeUpload();
-    }
-  }
-  
-  function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
-  }
-  $('.image-upload-wrap').bind('dragover', function () {
-          $('.image-upload-wrap').addClass('image-dropping');
-      });
-      $('.image-upload-wrap').bind('dragleave', function () {
-          $('.image-upload-wrap').removeClass('image-dropping');
-  });
-  
+  if (input.files && input.files[0]) {
 
-  const upload = (file) =>{
-    let formData = new FormData();
-    formData.append("imgfile", file);
-    var paragraph = document.getElementById("prediction");
-    fetch('http://localhost:81/classify_image/', { // Your POST endpoint
-      method: 'POST',
-      headers: {
-        // Content-Type may need to be completely **omitted**
-        // or you may need something
-      },
-      body: formData // This is your file object
-    }).then(
-      response => response.json() // if the response is a JSON object
-    )
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+    upload(input.files[0])
+
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+  $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+  $('.image-upload-wrap').removeClass('image-dropping');
+});
+
+
+const upload = (file) => {
+  let formData = new FormData();
+  formData.append("imgfile", file);
+  var paragraph = document.getElementById("prediction");
+  fetch('http://localhost:9100/classify_image/', { // Your POST endpoint
+    method: 'POST',
+    headers: {
+      // Content-Type may need to be completely **omitted**
+      // or you may need something
+    },
+    body: formData // This is your file object
+  }).then(
+    response => response.json() // if the response is a JSON object
+  )
     .then(
-      success => {console.log(success['label']) // Handle the success response object
-      label = success['label'];
-      id_image = success['id'];
-      var text = document.createTextNode("This building looks like " + label +" style")
-      paragraph.appendChild(text)
-    }).catch(
-      error => console.log(error) // Handle the error response object
-    );
-  };
+      success => {
+        console.log(success['label']) // Handle the success response object
+        label = success['label'];
+        id_image = success['id'];
+        var text = document.createTextNode("This building looks like " + label + " style")
+        paragraph.appendChild(text)
+      }).catch(
+        error => console.log(error) // Handle the error response object
+      );
+};
 
 
-  async function uploadNewClass(){
-    var newLabel = document.getElementById("new_label");
-    var value = newLabel.value;
-    console.log("New class: "+value+ " ID image: "+id_image)
-    fetch('http://localhost:9100/feedback_class/?id_img='+parseInt(id_image)+'&new_class='+parseInt(value), {
+async function uploadNewClass() {
+  var newLabel = document.getElementById("new_label");
+  var value = newLabel.value;
+  console.log("New class: " + value + " ID image: " + id_image)
+  fetch('http://localhost:9100/feedback_class/?id_img=' + parseInt(id_image) + '&new_class=' + parseInt(value), {
     method: 'PUT',
     headers: {},
-    }).then(
-      response => {
-        console.log(response.status);
-        response.json();
-        if (response.status == '200')
-          $("#success").addClass("show");
-        else
-          $("#fail").addClass("show");
-      }
-    ).then(
-      data => console.log(data)
-    ).then(
-      success => {console.log("Success: " +success);
+  }).then(
+    response => {
+      console.log(response.status);
+      response.json();
+      if (response.status == '200')
+        $("#success").addClass("show");
+      else
+        $("#fail").addClass("show");
+    }
+  ).then(
+    data => console.log(data)
+  ).then(
+    success => {
+      console.log("Success: " + success);
     }).catch(
-      error => {console.log("error:"+error);
-      $("#fail").addClass("show");
-    });
-  }
+      error => {
+        console.log("error:" + error);
+        $("#fail").addClass("show");
+      });
+}
 
-  $(function() {
-    $(".btn-close").click(function() {
-       $(this).closest(".alert").hide();         
-   });
+$(function () {
+  $(".btn-close").click(function () {
+    $(this).closest(".alert").hide();
+  });
 });
