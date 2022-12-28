0df8f30f26aa37eab679cb8559e1fe10d565c027
aiacovazzi <ar.iacovazzi@gmail.com>
2022-12-28 18:24:03 +0100

    locustfile created

---
+++ src/tests/locustfile.py
@@ -0,0 +1,10 @@
+import os
+from locust import HttpUser, task
+external =  os.path.join("data","external")
+class ClassifyLoadTest(HttpUser):
+    
+    @task
+    def classify(self):
+        test_file = os.path.join(external,"test_file", "pyramid.jpg")
+        self.client.post('/classify_image/',
+                        files={'imgfile': open(test_file, 'rb')})
