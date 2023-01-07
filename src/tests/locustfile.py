8cda88168d2311edd85ef83ea3af1713da967a00
Roberto <roblorus@icloud.com>
2023-01-07 16:11:11 +0100

    Locust select random test image

--- src/tests/locustfile.py
+++ src/tests/locustfile.py
@@ -1,10 +1,16 @@
 import os
+import random
+import glob
 from locust import HttpUser, task
-external =  os.path.join("data","external")
+
+external = os.path.join("data", "external")
+test_dir = os.path.join(external, "test_file")
+dir_dict = glob.glob(os.path.join(test_dir, "*"))
+upper_bound = len(dir_dict) - 1
+
+
 class ClassifyLoadTest(HttpUser):
-    
     @task
     def classify(self):
-        test_file = os.path.join(external,"test_file", "pyramid.jpg")
-        self.client.post('classify_image/',
-                        files={'imgfile': open(test_file, 'rb')})
+        test_file = dir_dict[random.randint(0, upper_bound)]
+        self.client.post("/classify_image/", files={"imgfile": open(test_file, "rb")})
